cf4d7443531899c5f166716546d8c22ec2b2f94b
nomad1072 <siddharthlanka@gmail.com>
2018-12-24 12:14:19 +0530

    :bug: fixes a bug in reading the files

--- index.js
+++ index.js
@@ -12,18 +12,21 @@ app.use(bodyParser.json())
 app.post('/transform_image', (req, res) => {
     let form = new formidable.IncomingForm();
     form.parse(req, async (err, fields, files) => {
-        console.log('Fields: <<<<', files);
         try {
-            let val = await jimp.read(files['test_image'].path);
-            let mask = await jimp.read(path.join(__dirname, 'pics', 'white.jpg'));
-            mask = await mask.resize(500, 500);
-            val = await val.resize(250, 400);
-            // updated_mask = await updated_mask.invert();
-            // updated_mask.write(path.join(__dirname, 'updated_mask.jpg'))
-            // val.mask(mask, 0, 0);
-            // val.write(path.join(__dirname, 'test_image.jpg'));
-            mask.composite(val, 120, 50);
-            mask.write(path.join(__dirname, 'output_pics','test_image.jpg'));
+            const InputFiles = Object.keys(files);
+            InputFiles.forEach(async (file) => {
+                let val = await jimp.read(files[file].path);
+                let mask = await jimp.read(path.join(__dirname, 'pics', 'white.jpg'));
+                mask = await mask.resize(500, 500);
+                val = await val.resize(250, 400);
+                // updated_mask = await updated_mask.invert();
+                // updated_mask.write(path.join(__dirname, 'updated_mask.jpg'))
+                // val.mask(mask, 0, 0);
+                // val.write(path.join(__dirname, 'test_image.jpg'));
+                mask.composite(val, 120, 50);
+                mask.write(path.join(__dirname, 'output_pics','test_image.jpg'));
+            });
+            
             return res.status(200).send({success: true})
         } catch (e) {
             console.error(e);
