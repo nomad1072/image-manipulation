1ad5baed415d95eb570e01bcf9788bf8fe374626
nomad1072 <siddharthlanka@gmail.com>
2018-12-25 22:26:06 +0530

    :sparkles: adds a UI to upload files

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="header-content">
+              A simple utility app to mask your image onto a white background(500*500) 
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const jimp = require('jimp');
 const formidable = require('formidable');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+const clientBuildPath = path.join("client", "build");
+const assets = path.join(__dirname, "output_pics");
+app.use(express.static(path.join(__dirname, clientBuildPath)));
+app.use(express.static(assets));
+app.use("*", (req, res) => {
+    res.sendFile(path.join(clientBuildPath, "index.html"), { root: __dirname });
+});
 
-app.post('/transform_image', (req, res) => {
+app.post('/api/v1/transform_image', (req, res) => {
     let form = new formidable.IncomingForm();
     form.parse(req, async (err, fields, files) => {
         try {
@@ -33,7 +40,6 @@ app.post('/transform_image', (req, res) => {
             return res.status(404).send({error: e});
         }
     });
-
 });
 
 app.listen(port, () => {
