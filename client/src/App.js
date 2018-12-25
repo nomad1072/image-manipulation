5b6c510d30ad02d320eb88e1b4782e27e27cdbee
nomad1072 <siddharthlanka@gmail.com>
2018-12-26 00:36:50 +0530

    :zap: brings a UI to download generated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,54 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone'
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  componentDidMount() {
 
+const baseStyle = {
+  width: 400,
+  height: 200,
+  borderWidth: 2,
+  borderColor: '#666',
+  borderStyle: 'dashed',
+  borderRadius: 5,
+  
+};
+const activeStyle = {
+  borderStyle: 'solid',
+  borderColor: '#6c6',
+  backgroundColor: '#eee'
+};
+const rejectStyle = {
+  borderStyle: 'solid',
+  borderColor: '#c66',
+  backgroundColor: '#eee'
+};
+class App extends Component {
+  state = {
+    files: []
+  }
+  onDrop = async (acceptedFiles, rejectedFiles) => {
+    this.setState({files: this.state.files.concat(acceptedFiles)});
+    if(acceptedFiles.length) {
+      let formData = new FormData();
+      // formData.append("image", acceptedFiles[0]);
+      formData.set("test_image", acceptedFiles[0]);
+      console.log(formData.get("test_image"))
+      const res = await axios.post('/api/v1/transform_image', formData, {
+        responseType: "blob"
+      });
+      const blob = new Blob([res.data], {
+        type: acceptedFiles[0].type
+      });
+      const reportUrl = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = reportUrl;
+      link.setAttribute("download", acceptedFiles[0].name);
+      document.body.appendChild(link);
+      link.click();
+    } else {
+      alert('Please upload a valid file');
+    }
   }
   render() {
     return (
@@ -12,6 +57,32 @@ class App extends Component {
           <div className="header-content">
               A simple utility app to mask your image onto a white background(500*500) 
           </div>
+          <div className="drop-content">
+            <Dropzone accept="image/*" onDrop={this.onDrop}>
+              {({ getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject, acceptedFiles, rejectedFiles }) => {
+                let styles = {...baseStyle}
+                styles = isDragActive ? {...styles, ...activeStyle} : styles
+                styles = isDragReject ? {...styles, ...rejectStyle} : styles
+                      
+                return (
+                  <div 
+                    {...getRootProps()}
+                    style={styles}
+                  >
+                    <input {...getInputProps()} />
+                    <div>
+                      {isDragAccept ? 'Drop' : 'Drag'} files here...
+                    </div>
+                    {isDragReject && <div>Unsupported file type...</div>}
+                  </div>
+                )
+              }}
+            </Dropzone>
+          </div>
+          <div className="file-list">
+              
+          </div>
+          <div className="failed-content">If drag and drop is not supported, click the area to open the file upload dialog.</div>
         </header>
       </div>
     );
--- index.js
+++ index.js
@@ -3,43 +3,52 @@ const bodyParser = require('body-parser');
 const jimp = require('jimp');
 const formidable = require('formidable');
 const path = require('path');
+const fs = require('fs');
 
 const port = process.env.PORT || 4000;
 const app = express();
+const router = express.Router();
 
 app.use(bodyParser.json());
-const clientBuildPath = path.join("client", "build");
+app.use(bodyParser.urlencoded({extended: true}));
 const assets = path.join(__dirname, "output_pics");
-app.use(express.static(path.join(__dirname, clientBuildPath)));
 app.use(express.static(assets));
-app.use("*", (req, res) => {
-    res.sendFile(path.join(clientBuildPath, "index.html"), { root: __dirname });
-});
+
 
 app.post('/api/v1/transform_image', (req, res) => {
+    console.log('In controller')
     let form = new formidable.IncomingForm();
-    form.parse(req, async (err, fields, files) => {
+    form.parse(req, async (err, fields, files) => {  
         try {
-            const InputFiles = Object.keys(files);
-            InputFiles.forEach(async (file) => {
-                let val = await jimp.read(files[file].path);
-                let mask = await jimp.read(path.join(__dirname, 'pics', 'white.jpg'));
-                mask = await mask.resize(500, 500);
-                val = await val.resize(250, 400);
-                // updated_mask = await updated_mask.invert();
-                // updated_mask.write(path.join(__dirname, 'updated_mask.jpg'))
-                // val.mask(mask, 0, 0);
-                // val.write(path.join(__dirname, 'test_image.jpg'));
-                mask.composite(val, 120, 50);
-                mask.write(path.join(__dirname, 'output_pics','test_image.jpg'));
-            });
-            
-            return res.status(200).send({success: true})
+            let output_path = "";
+            const file = Object.keys(files);
+            let val = await jimp.read(files[file[0]].path);
+            let mask = await jimp.read(path.join(__dirname, 'pics', 'white.jpg'));
+            mask = await mask.resize(500, 500);
+            val = await val.resize(250, 400);
+            mask.composite(val, 120, 50);
+            output_path = path.join(__dirname, 'output_pics', files[file[0]].name);
+            name = files[file[0]].name;
+            await mask.write(output_path);
+            const rs = fs.createReadStream('output_path');
+            rs.pipe(res);
+            rs.on("error", () => {
+                return res.status(404).send({error: "File not generated"});
+            })
+            // res.sendFile(output_path);
         } catch (e) {
             console.error(e);
             return res.status(404).send({error: e});
         }
     });
+});
+// app.use('/api/v1', router);
+
+const clientBuildPath = path.join(__dirname, "client", "build");
+app.use(express.static(clientBuildPath));
+app.use("*", (req, res) => {
+    console.log('Here!');
+    res.sendFile(`/client/build/index.html`, { root: __dirname });
 });
 
 app.listen(port, () => {
